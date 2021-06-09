100f7eaedc4938e43e473a340eb9f233cda4aadc
Alternativy <vesdsdsesfaeaw@gmail.com>
2021-06-09 11:28:10 -0300

    agrego Javascript para boton random
    a pedido del negro resentido

---
+++ javascript.js
@@ -0,0 +1,253 @@
+console.log("ta linkeado master")
+archivos=
+[
+null,
+"abduzcan_eze.ogg",
+"abris_alternativy.ogg",
+"actualizando_sheky.ogg",
+"alfombra_produ.ogg",
+"amor_vargas.ogg",
+"anal_sheky.ogg",
+"angular_alternativy_trucha.ogg",
+"anido_negro.ogg",
+"aplausos_otros.ogg",
+"arrastrandose_fabi.ogg",
+"atras_fabi.ogg",
+"audios_lucio.ogg",
+"avengers_otros.ogg",
+"azucar_fabi.ogg",
+"a_eze.ogg",
+"bien_tarsero.ogg",
+"boludo_vargas.ogg",
+"bomba_produ.ogg",
+"bostezo_renzo.ogg",
+"botonera_alternativy.ogg",
+"botonera_fabi.ogg",
+"botonera_negro.ogg",
+"boulshradmrm_papi.ogg",
+"bueno_monti.ogg",
+"bueno_produ.ogg",
+"caballo_cele.ogg",
+"calamardo_sticker.mpeg",
+"calamardo_sticker.ogg",
+"callate_otros.ogg",
+"callate_produ.ogg",
+"callese_lucio.ogg",
+"capo_renzo.ogg",
+"carajo_alternativy.ogg",
+"celular_alternativy.ogg",
+"champions_otros.ogg",
+"charly_sheky.ogg",
+"chau_negro.ogg",
+"chicato_alternativy.ogg",
+"clase_vargas.ogg",
+"confirmo_juma.ogg",
+"copado_lucio.ogg",
+"creador_renzo.ogg",
+"culo_fabi.ogg",
+"dale_negro.ogg",
+"dale_vargas.ogg",
+"darkness_otros.ogg",
+"decis_produ.ogg",
+"dedo_negro.ogg",
+"dedo_sheky.ogg",
+"delechemostalati_otros.ogg",
+"delfin_sheky.ogg",
+"descargar_fabi.ogg",
+"desesperacion_laura.ogg",
+"despertate_otros.ogg",
+"digievolucion_otros.ogg",
+"discord_alternativy.ogg",
+"doler_fabi.ogg",
+"eructo_produ.ogg",
+"estilo_negro.ogg",
+"exacto_alternativy.ogg",
+"eze55bp_alternativy.ogg",
+"e_sheky.ogg",
+"fabian_alternativy.ogg",
+"fer_produ_alternativy.ogg",
+"fideos_renzo.ogg",
+"flaco_renzo.ogg",
+"flash_tarsero.ogg",
+"fortnite_alternativy.ogg",
+"fueron_negro.ogg",
+"futbol_otros.ogg",
+"futuro_sheky.ogg",
+"gargara_renzo.ogg",
+"gasti_sheky.ogg",
+"gatitos_fabi.ogg",
+"gatos_renzo.ogg",
+"gol_negro.ogg",
+"goma_sheky.ogg",
+"gracias_produ.ogg",
+"gracias_sheky.ogg",
+"gracias_tarsero.ogg",
+"gracias_vargas.ogg",
+"grito_lucio.ogg",
+"hablar_fabi.ogg",
+"hijoeputa_ema.ogg",
+"hola_vargas.ogg",
+"homoneigices_renzo.ogg",
+"huevo_lucio.ogg",
+"inutil_renzo.ogg",
+"joda_produ.ogg",
+"luna_otros.ogg",
+"malisimo_otros.ogg",
+"manguito_alternativy.ogg",
+"maquina_fabi.ogg",
+"matar_fabi.ogg",
+"materias_gasti.ogg",
+"medialunas_lucio.ogg",
+"mejor_fabi.ogg",
+"mensajes_sheky.ogg",
+"messi_otros.ogg",
+"metes_produ.ogg",
+"me_sheky.ogg",
+"milamama_fabi.ogg",
+"minas_alternativy.ogg",
+"moladora_alternativy.ogg",
+"nashe_coscu.mpeg",
+"navidad_fabi.ogg",
+"ndeah_fabi.ogg",
+"nestor_renzo.ogg",
+"newells_sheky.ogg",
+"nose_renzo.ogg",
+"no_produ.ogg",
+"no_renzo.ogg",
+"no_sheky.ogg",
+"no_vargas.ogg",
+"ojete_sheky.ogg",
+"okay_lucio.ogg",
+"orale_sheky.ogg",
+"pacman_fabi.ogg",
+"parapapapa_alternativy.ogg",
+"para_leo.ogg",
+"pasion_sheky_alternativy.ogg",
+"pata_fabi.ogg",
+"patin_sheky.ogg",
+"pelotudo_otros.ogg",
+"perdon_produ.ogg",
+"perturbador_otros.ogg",
+"pinza_dug.ogg",
+"piola_renzo.ogg",
+"piso_produ.ogg",
+"playboy_negro.ogg",
+"policia_produ.ogg",
+"programar_walter.ogg",
+"putos_vargas.ogg",
+"puto_negro.ogg",
+"ratatata_sheky.ogg",
+"reglas_alternativy.ogg",
+"reirme_produ.ogg",
+"remate_otros.ogg",
+"resumen_peligro.ogg",
+"risa_fabi.ogg",
+"risa_sheky.ogg",
+"ruta_sheky.ogg",
+"sabana_fabi.ogg",
+"sabes_walter.ogg",
+"satan_renzo.aac",
+"se_gasti.ogg",
+"sistema_gasti.ogg",
+"si_alternativy.ogg",
+"si_gasti.ogg",
+"si_negro.ogg",
+"si_renzo.ogg",
+"skry_otros.ogg",
+"sonido_eze.ogg",
+"sorete_renzo.ogg",
+"tiago_alternativy.ogg",
+"titan_fabi.ogg",
+"toyota_eze.ogg",
+"traduccion_renzo.ogg",
+"tripode_lucio.ogg",
+"trolazo_fabi.ogg",
+"trolo_otros.ogg",
+"uade_sheky.ogg",
+"ue_renzo.ogg",
+"uf_otros.ogg",
+"ulalala_produ.ogg",
+"vaca_fabi.ogg",
+"vamos_alternativy.ogg",
+"verdad_negro.ogg",
+"vieja_otros.ogg",
+"wey_sheky.ogg",
+"wtf_otros.ogg",
+"zanganos_fabi.ogg",
+];
+
+// sonido.src="abris_alternativy.ogg";
+let total=170;
+let random = Math.round(Math.random()*total);
+console.log(random);
+console.log('"'+archivos[random]+'"');
+let resultado = archivos[random];
+console.log(resultado);
+let sonido = new Audio();
+sonido.src=resultado;
+
+
+// let audioRandom = new Audio();
+// audioRandom = "'"+archivos[random]+"'"
+// console.log(audioRandom)
+// Cantidad de audios de cada sección;
+
+// let cantidadAlternativy=18;
+// let cantidadEze=4;
+// let cantidadFabi=23;
+// let cantidadGasti=4;
+// let cantidadLucio=8;
+// let cantidadNegro=12;
+// let cantidadProdu=16;
+// let cantidadRenzo=16;
+// let cantidadSheky=20;
+// let cantidadTarsero=3;
+// let cantidadVargas=8;
+// alfabetico=(cantidadAlternativy+cantidadEze+cantidadEze+cantidadGasti+cantidadLucio+cantidadNegro+cantidadProdu+cantidadRenzo+cantidadSheky+cantidadTarsero+cantidadVargas);
+// otros=total-alfabetico;
+// alert(alfabetico);
+// alert(otros);
+// alert(total);
+// if (true) {
+// document.getElementsByClassName("cantidadAlternativy").innerHTML = "cxsaxsa";
+// document.getElementsByClassName("cantidadAlfabetico").innerHTML = alfabetico;
+// document.getElementsByClassName("cantidadProdu").innerHTML = cantidadProdu;
+// }
+// felipe = prompt(9);
+// alert(felipe);
+// document.write(felipe)
+
+// "cantidadAlfabetico"
+// "cantidadAlternativy"
+// "cantidadEze"
+// "cantidadFabi"
+// "cantidadGasti"
+// "cantidadLucio"
+// "cantidadNegro"
+// "cantidadProdu"
+// "cantidadRenzo"
+// "cantidadSheky"
+// "cantidadTarsero"
+// "cantidadVargas"
+
+
+// audiosAlternativy=[
+// null,
+// "abris_alternativy.ogg",
+// "botonera_alternativy.ogg",
+// "celular_alternativy.ogg",
+// "chicato_alternativy.ogg",
+// "discord_alternativy.ogg",
+// "exacto_alternativy.ogg",
+// "eze55bp_alternativy.ogg",
+// "fer_produ_alternativy.ogg",
+// "fortnite_alternativy.ogg",
+// "manguito_alternativy.ogg",
+// "minas_alternativy.ogg",
+// "parapapapa_alternativy.ogg",
+// "pasion_sheky_alternativy.ogg",
+// "reglas_alternativy.ogg",
+// "si_alternativy.ogg",
+// "tiago_alternativy.ogg",
+// "vamos_alternativy.ogg",
+// ];
